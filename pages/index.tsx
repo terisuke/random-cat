c171bf3ca0380f6c58b7a74e795c7a441274bd63
terisuke <terisuke1115@gmail.com>
2024-04-28 19:03:33 +0900

    create app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,43 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
+import { NextPage } from "next";
+import { useEffect, useState } from "react";
+import styles from "./index.module.css"
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-);
+const IndexPage: NextPage = () => {
+    const [imageUrl, setImageUrl] = useState("");
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetchImage().then((newImage) => {
+            setImageUrl(newImage.url);
+            setLoading(false);
+        });
+    }, []);
+    const handleClick = async () => {
+        setLoading(true);
+        const newImage = await fetchImage();
+        setImageUrl(newImage.url);
+        setLoading(false);
+    };
+    return (
+        <div className={styles.page}>
+            <button onClick={handleClick} className={styles.button}>
+                他のわんこも見る
+            </button>
+            <div className={styles.frame}>
+                {loading || <img src={imageUrl} className={styles.img} />}
+            </div>
+        </div>);
+};
 
 export default IndexPage;
+
+type Image = {
+    url: string;
+};
+const fetchImage = async (): Promise<Image> => {
+    const res = await fetch("https://api.thedogapi.com/v1/images/search")
+    const images = await res.json();
+    console.log(images);
+    return images[0];
+};
+
